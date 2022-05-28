527f4bdabcf80ba88af3d9e2b8734fc865d26d44
Wagner Ribas <wagnerribas63@gmail.com>
2022-05-28 17:23:59 -0300

    validate request endpoint

--- auth-service/main.go
+++ auth-service/main.go
@@ -13,6 +13,12 @@ import (
 
 var hmacSampleSecret []byte
 
+type ValidateRequestDTO struct {
+	Action string `json:"action"`
+	UserID int    `json:"user_id"`
+	Role   string `json:"role"`
+}
+
 func main() {
 	repositories := repositories.New()
 
@@ -49,10 +55,45 @@ func main() {
 
 		tokenString, err := token.SignedString(hmacSampleSecret)
 
-		fmt.Println(user)
 		c.JSON(http.StatusOK, gin.H{
 			"token": tokenString,
 		})
+	})
+
+	r.POST("/auth/validateRequest", func(c *gin.Context) {
+		tokenString := c.Request.Header.Get("Authorization")
+		tokenString = tokenString[7:] // remove Bearer from token string
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+			return hmacSampleSecret, nil
+		})
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			return
+		}
+
+		var validateRequestDTO ValidateRequestDTO
+		if err := c.ShouldBindJSON(&validateRequestDTO); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			return
+		}
+
+		if validateRequestDTO.Action == "listPersonDebts" && validateRequestDTO.Role != "master" && validateRequestDTO.UserID != int(claims["user_id"].(float64)) {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{})
+
 	})
 
 	r.Run()
